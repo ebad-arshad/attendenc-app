63c40d029bcf38712796450e9f1c53aa6b2e76e9
ebad-arshad <m.ebadarshad2003@gmail.com>
2022-11-12 12:51:47 +0500

    login page firebase complete

---
+++ app.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDwCWxd2M5NdD7idagpOIqczYxL8RodCds",
+    authDomain: "test-38127.firebaseapp.com",
+    projectId: "test-38127",
+    storageBucket: "test-38127.appspot.com",
+    messagingSenderId: "1021886461867",
+    appId: "1:1021886461867:web:b325ddfc2aa2ac8d8104ee"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.onload = onAuthStateChanged(auth, (user) => {
+    if (user) {
+        location = "adminPanel/index.html";
+    } else {
+    }
+});
+
+
+// Login Function
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+const loginBtn = () => {
+    event.preventDefault();
+
+    signInWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            location = "adminPanel/index.html";
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+            console.log(errorMessage);
+            errorMessage === "Firebase: Error (auth/invalid-email)." ? swal("User not Found", "Please input correct email", "error") : swal("Password is Incorrect", "Please input correct password", "error");
+            // swal("User / Password Wrong", , "success");
+        });
+
+}
+
+window.loginBtn = loginBtn;
